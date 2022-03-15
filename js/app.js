af1944d8759791cd00145a58098a26c4010c178a
masud rana <masudranaacb@gmail.com>
2022-03-15 11:29:24 +0600

    all country name Add

--- js/app.js
+++ js/app.js
@@ -3,6 +3,20 @@ const loadAllCuntries=()=>{
     .then(res=>res.json())
     .then(data=>allCountries(data))
 }
-
+const allCountries=(countries)=>{
+    // console.log(countries)
+    const countriesHtml=countries.map(cuntry=>getCountry(cuntry))
+    const container=document.getElementById('countries')
+    container.innerHTML=countriesHtml.join(' ')
+}
+const getCountry=(cuntry)=>{
+    console.log(cuntry)
+    return `
+        <div class='country'>
+            <h3>${cuntry.name.common}</h3>
+        </div>
+    
+    `
+}
 
 loadAllCuntries()
